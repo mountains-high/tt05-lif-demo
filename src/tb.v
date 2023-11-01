74dce62348d9a52e3820cfed626675f5e07ac214
mountains-high <oceandi@ASSELs-MacBook-Pro.local>
2023-11-01 14:25:48 -0700

    tb.v is modified

--- src/tb.v
+++ src/tb.v
@@ -29,8 +29,6 @@ module tb ();
     wire [7:0] uio_oe;
 
     tt_um_lif tt_um_lif (
-
-    `endif
         .ui_in      (ui_in),    // Dedicated inputs
         .uo_out     (uo_out),   // Dedicated outputs
         .uio_in     (uio_in),   // IOs: Input path
