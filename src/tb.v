4d0ee216229eb937e5f1f0cd5fbb9a0fe85e28e6
mountains-high <oceandi@ASSELs-MacBook-Pro.local>
2023-11-01 14:40:17 -0700

    tb.v is modified

--- src/tb.v
+++ src/tb.v
@@ -1,11 +1,6 @@
 `default_nettype none
 `timescale 1ns/1ps
 
-/*
-this testbench just instantiates the module and makes some convenient wires
-that can be driven / tested by the cocotb test.py
-*/
-
 // testbench is controlled by test.py
 module tb ();
 
