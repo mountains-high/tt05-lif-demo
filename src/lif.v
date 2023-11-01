bf67e5a7898e225718fd68f15c2be0299c7b477a
mountains-high <oceandi@ASSELs-MacBook-Pro.local>
2023-11-01 13:33:28 -0700

    lif.v updates

--- src/lif.v
+++ src/lif.v
@@ -28,32 +28,27 @@
 */
 `default_nettype none
 
-module lif (
+module lif ( 
     input wire [7:0] current,
     input wire       clk,
     input wire       rst_n,
     output wire      spike,
     output reg [7:0] state
 );
+    reg [7:0] threshold;
+    wire [7:0] next_state;
 
-reg [7:0] state, threshold;
-wire [7:0] next_state;
-
-always @(posedge clk) begin
-    if (!rst_n) begin
-        state <=0;
-        threshold <=127;
-    end else begin
-        state <= next_state;
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            state <= 0;
+            threshold <= 230;
+        end else begin
+            state <= next_state;
+        end
     end
-end
-    //next_state logic
-    assign next_state = current + (state>>1);
 
-    //spking logic #no reset mechanism
-    assign spike = (spike>=threshold);
-    assign next_state = (spike ? 0 : current) + (spike ? 0: (state >> 1)+(state>>2)+(state>>3));
-
+    // next_state logic and spiking logic
+    assign spike = (state >= threshold);
+    assign next_state = (spike ? 0 : current) + (spike ? 0 : (state >> 1)+(state >> 2)+(state >> 3));
 
 endmodule
-
